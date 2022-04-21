5277c5aa505b5ec051609c439213c6f8db7752d4
Vladimir Chistyakov <v.chistyakov@integrasources.com>
2022-04-21 16:58:17 +0700

    feat(nvim): add docker lsp support

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -80,6 +80,7 @@ return require('packer').startup(function()
 
       lspconfig.cmake.setup{}
       lspconfig.pylsp.setup{}
+      lspconfig.dockerls.setup{}
 
       vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', {noremap = true, silent = true})
       vim.api.nvim_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', {noremap = true, silent = true})
