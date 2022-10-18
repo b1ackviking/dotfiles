0c1e970328758c688c1b74c39e50debdee896d99
Vladimir Chistyakov <b1ackviking@outlook.com>
2022-10-18 15:14:05 +0700

    fix(nvim): set lsp capabilities correctly

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -65,8 +65,7 @@ packer.startup(function()
   packer.use {
     'neovim/nvim-lspconfig',
     config = function()
-      local capabilities = require('cmp_nvim_lsp').update_capabilities(
-        vim.lsp.protocol.make_client_capabilities())
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
       local lspconfig = require 'lspconfig'
       lspconfig.clangd.setup {
         cmd = {
