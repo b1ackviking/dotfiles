a46895e513fea4255f4be017316d9cbafe91b730
Vladimir Chistyakov <b1ackviking@outlook.com>
2024-04-15 21:14:28 +0700

    chore(nvim): remove duplicated defaults for gitsigns

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -194,15 +194,10 @@ require('lazy').setup({
       require 'gitsigns'.setup {
         current_line_blame = true,
         current_line_blame_opts = {
-          virt_text = true,
-          virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
           delay = 300,
-          ignore_whitespace = false,
         },
         current_line_blame_formatter = '<author>, <author_time:%d %b %Y %H:%M> - <summary>',
-        yadm = {
-          enable = true
-        }
+        yadm = { enable = true }
       }
     end
   },
