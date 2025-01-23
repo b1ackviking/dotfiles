ed8b44ca905d8a641101e38ab4c095c32b10c268
Vladimir Chistyakov <b1ackviking@outlook.com>
2025-01-23 07:25:20 +0700

    fix(nvim): change vim.loop to vim.uv
    vim.uv replaces vim.loop starting from neovim version 0.10

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -84,9 +84,19 @@ vim.api.nvim_create_autocmd({ 'BufEnter' }, {
 })
 
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({ 'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git', '--branch=stable',
+if not vim.uv.fs_stat(lazypath) then
+  local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable',
     lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out,                            "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
